4407c5a66c87ec3694d5d5fd6f424400610f919c
Corky <corky@corkysdevshop.com>
2023-10-19 14:56:19 -0600

    starting chapter 2

---
+++ src/test/java/com/javapractice/JavapracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapractice.javapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavapracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
